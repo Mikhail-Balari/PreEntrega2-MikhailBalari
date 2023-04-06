7a106932a7f2a9c9ed148d9d592dd88f8dc82933
Mikhail-Balari <balarimikhail@gmail.com>
2023-04-06 00:31:55 -0300

    Este es el primer commit con todos los cambios

--- api/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Module, Pin
-
-
-class PinForm(forms.ModelForm):
-    class Meta:
-        model = Pin
-        fields = ['is_used']
--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('machines', views.machine_list, name='machine-list'),
-    path('machines/<int:id>', views.machine, name='machine-detail'),
-    path('module/<int:id>', views.module, name='module-detail'),
-]
